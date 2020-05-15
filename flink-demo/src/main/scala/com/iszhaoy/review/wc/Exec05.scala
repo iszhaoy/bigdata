bb830d0de2bc787d47be825ce00f58424b42f2ee
iszhaoy <iszhaoy@163.com>
2020-05-15 09:40:45 +0800

    commit

--- flink-demo/src/main/scala/com/iszhaoy/review/wc/Exec05.scala
+++ flink-demo/src/main/scala/com/iszhaoy/review/wc/Exec05.scala
@@ -1,11 +1,13 @@
 package com.iszhaoy.review.wc
 
+import org.apache.flink.api.common.functions.ReduceFunction
+import org.apache.flink.api.common.state.{ReducingStateDescriptor, ValueState, ValueStateDescriptor}
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
 import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows
-import org.apache.flink.streaming.api.windowing.triggers.{Trigger, TriggerResult}
+import org.apache.flink.streaming.api.windowing.triggers.{CountTrigger, Trigger, TriggerResult}
 import org.apache.flink.streaming.api.windowing.windows.{GlobalWindow, TimeWindow}
 import org.apache.flink.util.Collector
 
@@ -24,47 +26,56 @@ object Exec05 {
     env.setParallelism(1)
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
     env.fromElements(
-      ("Yang", "English", 95),
       ("Zhang", "Math", 62),
-      ("Li", "Math", 87),
+      ("Yang", "English", 95),
       ("Zhang", "English", 92),
+      ("Li", "Math", 87),
       ("Yang", "Math", 90)
     ).keyBy(_._1)
       // 全局窗口，会将所有的数据放在一个窗口中，需要自己制定触发器，
       .window(GlobalWindows.create())
-      .trigger(new MyTrigger(2))
-      .process(new ProcessWindowFunction[(String, String, Int), (String, String, Int), String, GlobalWindow] {
-        override def process(key: String, context: Context, elements: Iterable[(String, String, Int)], out: Collector[
-          (String, String, Int)]): Unit = {
-          var sum = 0
-          for (elem <- elements) {
-            sum += elem._3
-          }
-          out.collect((key, "sum", sum))
-        }
-      })
+//      .trigger(new MyTrigger2(2))
+      .trigger(CountTrigger.of(2L))
+      .reduce((x,y) => (x._1,"sum",x._3 + y._3))
+//      .process(new ProcessWindowFunction[(String, String, Int), (String, String, Int), String, GlobalWindow] {
+//        override def process(key: String, context: Context, elements: Iterable[(String, String, Int)], out: Collector[
+//          (String, String, Int)]): Unit = {
+//          var sum = 0
+//          for (elem <- elements) {
+//            sum += elem._3
+//          }
+//          out.collect((key, "sum", sum))
+//        }
+//      })
       .print("test")
 
     env.execute("exec05")
   }
 }
 
+class Sum() extends ReduceFunction[Int] {
+  override def reduce(t: Int, t1: Int): Int = t + t1
+}
+
 class MyTrigger2(num: Int) extends Trigger[(String, String, Int), GlobalWindow] {
-  var flag = 0
+  // trigger中定义描述器
+  val countDescriptor = new ReducingStateDescriptor[Int]("count", new Sum, classOf[Int])
 
   override def onElement(element: (String, String, Int), timestamp: Long, window: GlobalWindow, ctx: Trigger.TriggerContext)
   : TriggerResult = {
-    if (flag == num) {
-      flag = 0
-      println(s"fire     ===============       $flag     $element")
+    // 通过getPartitionState,传入描述器获取分区状态
+    val countState = ctx.getPartitionedState(countDescriptor)
+    //计数状态加1
+    countState.add(1)
+    if (countState.get() == num) {
+      countState.clear()
+      println(s"fire     ===============       ${countState.get()}     $element")
       TriggerResult.FIRE_AND_PURGE
     } else {
-      flag += 1
-      println(s"continue ===============       $flag     $element")
+      println(s"continue ===============      ${countState.get()}     $element")
       TriggerResult.CONTINUE
     }
   }
-
   override def onProcessingTime(time: Long, window: GlobalWindow, ctx: Trigger.TriggerContext): TriggerResult =
     TriggerResult.CONTINUE
 
@@ -74,7 +85,9 @@ class MyTrigger2(num: Int) extends Trigger[(String, String, Int), GlobalWindow] 
   override def clear(window: GlobalWindow, ctx: Trigger.TriggerContext): Unit = {}
 }
 
-
+/**
+ * 这种结果正确，么看懂。。。。
+ * */
 class MyTrigger(num: Int) extends Trigger[(String, String, Int), GlobalWindow] {
   // 使用map保存用户的课程数量
   val map = new mutable.HashMap[String, Int]()
